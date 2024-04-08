da54ac4ed6ea70d293b3ac31f0146ff7f2d6b49d
CalmaxBenard <calmaxbenard@gmail.com>
2024-04-08 15:07:30 +0300

    add a method to capture voice

--- speech.py
+++ speech.py
@@ -1,12 +1,23 @@
 # Imports
 import speech_recognition as sr
+import os
+from tkinter import messagebox
 from gtts import gTTS
+from translator import LanguageTranslator
 
 
-class SpeechTranslator:
-    def __init__(self):
+class SpeechTranslator(LanguageTranslator):
+    def __init__(self, root):
+        super().__init__(root)
         self.recognize = sr.Recognizer()
         self.mc = sr.Microphone()
 
-
-
+    def capture_voice(self):
+        with self.mc as source:
+            messagebox.showinfo("Start Key", "Say hello to start translation.")
+            os.environ[
+                "GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\pc\Downloads\cool-monolith-419708-7db501814ea0.json"
+            self.recognize.adjust_for_ambient_noise(source, duration=0.5)
+            audio = self.recognize.listen(source=source)
+            my_text = self.recognize.recognize_google_cloud(audio)
+            my_text = my_text.lower()
