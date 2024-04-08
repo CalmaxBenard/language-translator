bfd3036efd6b05bf6737a59f5b98b4bb661a4fe7
CalmaxBenard <calmaxbenard@gmail.com>
2024-04-08 14:48:58 +0300

    initialize SpeechTranslator class

--- speech.py
+++ speech.py
@@ -3,3 +3,10 @@ import speech_recognition as sr
 from gtts import gTTS
 
 
+class SpeechTranslator:
+    def __init__(self):
+        self.recognize = sr.Recognizer()
+        self.mc = sr.Microphone()
+
+
+
