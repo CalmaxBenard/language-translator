47c4ee98962f59c0b6df78b7a9292d71f18036f8
CalmaxBenard <calmaxbenard@gmail.com>
2024-04-08 08:52:42 +0300

    import Frame and Ridge

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 # Import libraries
-from tkinter import Tk
-from tkinter import messagebox
-from tkinter import ttk
+from tkinter import Tk, Frame, messagebox, ttk, RIDGE
 from googletrans import Translator
 
 # Screen setup
 root = Tk()
 root.title("Language Translator")
 root.geometry("590x370")
+
+# Style the screen
+frame = Frame(root, width=590, height=370, relief=RIDGE, borderwidth=5, bg="#F7DC6F")
+frame.place(x=0, y=0)
+
 
 root.mainloop()
 
