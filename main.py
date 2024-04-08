f1ec3efa7664d1b33ff3cb2e1deea7587d412d9f
CalmaxBenard <calmaxbenard@gmail.com>
2024-04-08 08:57:26 +0300

    import Label to set screen title

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Import libraries
-from tkinter import Tk, Frame, messagebox, ttk, RIDGE
+from tkinter import Tk, Frame, messagebox, ttk, RIDGE, Label
 from googletrans import Translator
 
 # Screen setup
@@ -10,6 +10,10 @@ root.geometry("590x370")
 # Style the screen
 frame = Frame(root, width=590, height=370, relief=RIDGE, borderwidth=5, bg="#F7DC6F")
 frame.place(x=0, y=0)
+
+# Set title/label on screen
+Label(root, text="Language Translator", font=("Helvetica", 20, "bold"), fg="black", bg="#F7DC6F").pack(pady=10)
+
 
 
 root.mainloop()
